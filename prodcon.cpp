f208d87df5870393517647a91c045424e181cb68
Arsh <arsh.narkhede@gmail.com>
2022-10-25 22:40:43 -0600

    commit before testing multithreading

--- prodcon.cpp
+++ prodcon.cpp
@@ -9,6 +9,7 @@ using namespace std;
 //MUTEX Variables
 pthread_cond_t queueCond = PTHREAD_COND_INITIALIZER;
 pthread_mutex_t queueMutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t queueCountMutex = PTHREAD_MUTEX_INITIALIZER;
 
 //tands.cpp functions
 extern void Trans(int n);
@@ -16,11 +17,10 @@ extern void Sleep(int n);
 
 unsigned int workCount = 0;
 unsigned int queueLimit = 0;
+// work queue.. no size restrictions yet, implemented later in code
+queue<int> workQueue;
 
 void operations(int nthreads) {
-	// work queue.. no size restrictions yet, implemented later in code
-	queue<string> workQueue;
-
 	//creating threads
 	pthread_t threads[nthreads];
 
@@ -41,31 +41,78 @@ void operations(int nthreads) {
 	int cmdType;
 	while(getline(cin, workCommand)) {
 		cmdType = getCommandType(workCommand);
-		try {
-			if(cmdType == 0) {
-				Trans(stoi(workCommand.substr(1)));
+		if(cmdType == 0) {
+			int n = 0;
+			try {
+				n = stoi(workCommand.substr(1));
 			}
-			else if(cmdType == 1) {
-				Sleep(stoi(workCommand.substr(1)));
+			catch(...) {
+				cout << "Invalid arguments\n";
+			}
+
+			//adding work to queue
+			pthread_mutex_lock(&queueMutex);
+			if (workCount > queueLimit) {
+				//log ask using n as -1 as 'n' does not exist for ask
+				outputAndCalculation(*threadID, "Ask", workCount, n);
+				//wait for consumer to consume
+				pthread_cond_wait(&queueCond, &queueMutex);
 			}
 			else {
-				cout << "Invalid Command\n";
+				//add work to queue to consume
+				pthread_mutex_lock(&queueCountMutex);
+				workQueue.push(n);
+				workCount++;
+				pthread_mutex_unlock(&queueCountMutex);
+				//signal consumer that work has been added
+				pthread_cond_signal(&queueCond);
+				outputAndCalculation(0, "Work", workCount, n);
+			}
+			pthread_mutex_unlock(&queueMutex);
+		}
+		else if(cmdType == 1) {
+			try {
+				Sleep(stoi(workCommand.substr(1)));
+			}
+			catch(...) {
+				cout << "Invalid arguments\n";
 			}
 		}
-		//cath all exceptions that could be raised by stoi
-		catch(...) {
-			cout << "Please enter as T<int> or S<int>\n";
+		else {
+			cout << "Invalid commands\n";
 		}
 	}
+	pthread_mutex_lock(&queueMutex);
+	outputAndCalculation(0, "End", workCount, -1);
+	pthread_mutex_unlock(&queueMutex);
 }
 
 //consumer thread functions
 void* consumer(void* args_p) {
 	int* threadID = (int*) args_p;
+	int n = 0;
 	pthread_mutex_lock(&queueMutex);
-	while (workCount == 0) {
-		outputAndCalculation(*threadID, "Ask", workCount, 0);
+	if (workCount == 0) {
+		//log ask using n as -1 as 'n' does not exist for ask
+		outputAndCalculation(*threadID, "Ask", workCount, -1);
+		//wait for the producer to add work
 		pthread_cond_wait(&queueCond, &queueMutex);
+	}
+	else {
+		//get int value from queue (FIFO) and removing from active work count
+		pthread_mutex_lock(&queueCountMutex);
+		n = workQueue.front();
+		workQueue.pop();
+		workCount--;
+		pthread_mutex_unlock(&queueCountMutex);
+		//signal that element from queue was removed
+		pthread_cond_signal(&queueCond);
+		//command received
+		outputAndCalculation(*threadID, "Receive", workCount, n);
+		//execute command
+		Trans(n);
+		//command completed
+		outputAndCalculation(*threadID, "Complete", workCount, n);
 	}
 	pthread_mutex_unlock(&queueMutex);
     return NULL;
@@ -98,10 +145,12 @@ int main (int argc, char *argv[]) {
 	if (tid != 0) {
 		outFile = "prodcon." + to_string(tid) + ".log";
 	}
+	//log every STDOUT to file
 	loggedToFile(outFile);
+	//threads operations
 	operations(nthreads);
+	//print the summary
 	summaryOutput(nthreads);
 	exit(0);
-
 	return 0;
 }
